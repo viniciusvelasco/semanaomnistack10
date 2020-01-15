4f3953212a0c1e26e98973a6008f2239e6570fd1
Vinicius Alves Velasco <vinicius.velasco@amaggi.com.br>
2020-01-15 00:02:03 -0400

    Criação do backend

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-w4oyg.mongodb.net/week10?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json());
+app.use(routes);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+app.listen(3333);
